76d6bf870272bc4e2aff628e5463e7d1b449f317
Santiago Castro <santi.1410@hotmail.com>
2019-05-26 21:07:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# [Personal website](https://bryant1410.github.io/)
+
+This is [my personal website](https://bryant1410.github.io/). The template and design was originally copied from
+[Start Bootstrap - Resume](https://github.com/BlackrockDigital/startbootstrap-resume).
---
+++ js/index.js
@@ -0,0 +1,23 @@
+(function ($) {
+  "use strict";
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  $('.js-scroll-trigger').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  $('body').scrollspy({target: '#sideNav'});
+
+})(jQuery);
